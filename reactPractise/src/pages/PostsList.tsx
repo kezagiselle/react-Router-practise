ad2180c7fad7e18db36a11abbcae7b985f449370
kezagiselle <kezagiselle776@gmail.com>
2026-01-08 14:52:28 +0200

    feat: adding photos

--- reactPractise/src/pages/PostsList.tsx
+++ reactPractise/src/pages/PostsList.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import p3 from '../img/p3.jpg'
+import p4 from '../img/p4.jpg'
+import p5 from '../img/p5.jpg'
+import p6 from '../img/p6.jpg'
+import p8 from '../img/p8.jpg'
+import p1 from '../img/p1.jpg'
 
 function postsList() {
   return (
