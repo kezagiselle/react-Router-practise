2ced2083cc4a78589718636ce79eae4218987522
kezagiselle <kezagiselle776@gmail.com>
2026-01-08 13:38:34 +0200

    feat: adding a header

---
+++ reactPractise/src/pages/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold italic flex justify-center mt-15">Fav Cities</h1>
+    </div>
+  )
+}
+
+export default Header
