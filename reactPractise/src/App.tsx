49021aaf098746fd535c02f7a440b49fa668ffa6
kezagiselle <kezagiselle776@gmail.com>
2026-01-08 14:16:46 +0200

    feat: working on the rootLayout

--- reactPractise/src/App.tsx
+++ reactPractise/src/App.tsx
@@ -4,6 +4,10 @@ import PostLayout from './pages/PostLayout'
 import PostsList from './pages/postsList'
 import PostDetails from './pages/PostDetails'
 import PostComments from './pages/PostComments'
+import RootLayout from './pages/RootLayout'
+import Home from './pages/Home'
+import About from './pages/About'
+import ContactUs from './pages/ContactUs'
 
 
 function App() {
@@ -12,8 +16,13 @@ function App() {
    <BrowserRouter>
    <Routes>
  {/* root layout */}
- 
-     <Route path='/posts' element={<PostLayout />}>
+    <Route path='/' element={<RootLayout />} />
+    <Route index element={<Home />} />
+    <Route path='about' element={<About />} />
+    <Route path='contact' element={<ContactUs />} />
+
+    {/* post layout */}
+     <Route path='posts' element={<PostLayout />}>
      <Route index element={<PostsList />} />
      <Route path=':postId' element={<PostDetails />} />
      <Route path=':postId/comments' element={<PostComments />} />
--- reactPractise/src/pages/PostLayout.tsx
+++ reactPractise/src/pages/PostLayout.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import Header from './Header'
+
 import { Outlet } from 'react-router-dom'
 
 function PostLayout() {
   return (
     <div>
-      <Header />
+     
       <Outlet />
     </div>
   )
