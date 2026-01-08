97b74590e816b7141dfc2743d63bc84262ded67f
kezagiselle <kezagiselle776@gmail.com>
2026-01-08 14:23:18 +0200

    feat: adding navigation links

--- reactPractise/src/pages/Home.tsx
+++ reactPractise/src/pages/Home.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Header from './Header'
+import { NavLink } from 'react-router-dom'
 
 function Home() {
   return (
     <div>
-      
+       <Header />
+
+       <div className="text-2xl flex justify-end gap-5 mr-50">
+       <NavLink to='/about'>About</NavLink>
+       <NavLink to='/contact'>Contact</NavLink>
+       </div>
+    
     </div>
   )
 }
--- reactPractise/src/pages/RootLayout.tsx
+++ reactPractise/src/pages/RootLayout.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import Header from './Header'
+
 import { Outlet } from 'react-router-dom'
 
 function RootLayout() {
   return (
     <div>
-      <Header />
+     
       <Outlet />
     </div>
   )
