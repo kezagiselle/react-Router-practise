7f3686169b42113053e0733bbb5ba646a73bd0ec
kezagiselle <kezagiselle776@gmail.com>
2026-01-08 14:39:53 +0200

    feat: finished adding the colors

--- reactPractise/src/pages/Home.tsx
+++ reactPractise/src/pages/Home.tsx
@@ -8,9 +8,9 @@ function Home() {
        <Header />
 
        <div className="text-2xl flex justify-end gap-10 mr-80 mt-5 font-semibold ">
-       <NavLink to='/about' className=" hover:text-blue-500 transform hover:scale-130 transition duration-300">About</NavLink>
-       <NavLink to='/posts' className=" hover:text-blue-500transform hover:scale-130 transition duration-300">Posts</NavLink>
-       <NavLink to='/contact' className="hover:text-blue-500 transform hover:scale-130 transition duration-300">Contact</NavLink>
+       <NavLink to='/about' className=" hover:text-green-600 transform hover:scale-130 transition duration-300">About</NavLink>
+       <NavLink to='/posts' className=" hover:text-green-600 transform hover:scale-130 transition duration-300">Posts</NavLink>
+       <NavLink to='/contact' className="hover:text-green-600 transform hover:scale-130 transition duration-300">Contact</NavLink>
        </div>
     
     </div>
